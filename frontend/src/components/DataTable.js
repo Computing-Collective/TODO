575e88f1e66f7cd8c7838e40d97c4b33b9c967a4
Divy Patel <82171007+divy-07@users.noreply.github.com>
2023-01-21 15:46:30 -0800

    Conver to 4 spaces

--- frontend/src/components/DataTable.js
+++ frontend/src/components/DataTable.js
@@ -4,14 +4,14 @@ import { DataGrid } from '@mui/x-data-grid';
 
 
 export default function DataTable(props) {
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <DataGrid
-        rows={props.tempRows}
-        columns={props.tempColumns}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
-  );
+    return (
+        <div style={{ height: 'inherit', width: '100%' }}>
+            <DataGrid
+                rows={props.rows}
+                columns={props.cols}
+                // rowsPerPageOptions={[5]}
+                checkboxSelection
+            />
+        </div>
+    );
 }
