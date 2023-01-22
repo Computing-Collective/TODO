470ace59335e1dd52c98fa28511f4660dc699dfe
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-01-22 10:08:07 -0800

    Catch Piazza API rate limiting

--- backend/piazza/api.py
+++ backend/piazza/api.py
@@ -39,66 +39,69 @@ def piazza_api(include_discussions=False, term="Winter Term 2 2023"):
     for id, name in class_id_name_dict.items():
         course = p.network(id)
         feed = course.iter_all_posts(limit=10)
-        for post in feed:
-            # announcement or no?
-            instructor_note: bool = "instructor-note" in post["tags"]
-            # unique id
-            post_id: str = "p" + post["id"]  # post id
-            # created time
-            created: str = post[
-                "created"
-            ]  # time created ISO 691? '2023-01-20T17:58:27Z' as a **string**
-            original_post_body: str = post["history"][0]["content"]  # in html format
-            post_id = post["change_log"][0]["data"]
+        try:
+            for post in feed:
+                # announcement or no?
+                instructor_note: bool = "instructor-note" in post["tags"]
+                # unique id
+                post_id: str = "p" + post["id"]  # post id
+                # created time
+                created: str = post[
+                    "created"
+                ]  # time created ISO 691? '2023-01-20T17:58:27Z' as a **string**
+                original_post_body: str = post["history"][0]["content"]  # in html format
+                post_id = post["change_log"][0]["data"]
 
-            # get post name
-            if post["history"][0]["anon"] == "no":
-                user_id = post["history"][0]["uid"]
-                user = course.get_users([user_id])
-                original_poster: str = user[0]["name"]
-            else:
-                original_poster = "Anonymous"
+                # get post name
+                if post["history"][0]["anon"] == "no":
+                    user_id = post["history"][0]["uid"]
+                    user = course.get_users([user_id])
+                    original_poster: str = user[0]["name"]
+                else:
+                    original_poster = "Anonymous"
 
-            # type of message
-            msg_type: str = post["type"]
+                # type of message
+                msg_type: str = post["type"]
 
-            subject: str = post["history"][0]["subject"]
-            # get course
-            course_arr = name.split(" ")
-            course_name = course_arr[0] + " " + course_arr[1]
-            course_name = get_course_nickname(course_name)
-            # get link
-            post_num: int = post["nr"]
-            link: str = f"https://piazza.com/class/{id}/post/{post_num}"
+                subject: str = post["history"][0]["subject"]
+                # get course
+                course_arr = name.split(" ")
+                course_name = course_arr[0] + " " + course_arr[1]
+                course_name = get_course_nickname(course_name)
+                # get link
+                post_num: int = post["nr"]
+                link: str = f"https://piazza.com/class/{id}/post/{post_num}"
 
-            # store into classes
-            if instructor_note:
-                announcements_to_add.append(
-                    AnnouncementMessage(
-                        identifier="p" + post_id,
-                        title=subject,
-                        poster_name=original_poster,
-                        course=course_name,
-                        link=link,
-                        # remove the last char to fit iso format
-                        post_date=datetime.datetime.fromisoformat(created[:19]),
-                        message=original_post_body,
-                        mark_read=False,
-                    )
-                )
-            else:
-                if include_discussions:
-                    discussions_to_add.append(
-                        DiscussionPost(
-                            identifier=post_id,
-                            poster_name=original_poster,
+                # store into classes
+                if instructor_note:
+                    announcements_to_add.append(
+                        AnnouncementMessage(
+                            identifier="p" + post_id,
                             title=subject,
-                            post_type=msg_type,
-                            description=original_post_body,
+                            poster_name=original_poster,
+                            course=course_name,
+                            link=link,
                             # remove the last char to fit iso format
                             post_date=datetime.datetime.fromisoformat(created[:19]),
+                            message=original_post_body,
+                            mark_read=False,
                         )
                     )
+                else:
+                    if include_discussions:
+                        discussions_to_add.append(
+                            DiscussionPost(
+                                identifier=post_id,
+                                poster_name=original_poster,
+                                title=subject,
+                                post_type=msg_type,
+                                description=original_post_body,
+                                # remove the last char to fit iso format
+                                post_date=datetime.datetime.fromisoformat(created[:19]),
+                            )
+                        )
+        except:
+            pass
 
     add_to_database(announcements_to_add, "announcements")
     if include_discussions:
