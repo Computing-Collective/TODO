f5c93902df6ba0d83965fd105404c7de5167959d
im-calvin <kelvinwong0519@gmail.com>
2023-01-22 11:21:15 -0800

    broke

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,84 +6,72 @@ import Stack from "@mui/material/Stack";
 import axios, { isCancel, AxiosError } from "axios";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import IconButton from "@mui/material/IconButton";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import { Announcement } from "@mui/icons-material";
 import Paper from "@mui/material/Paper";
 import ExpandedView from "./components/Expanded";
 
 function App() {
   const [view, setView] = useState("announcements");
-  // const [rows, setRows] = useState([]); // holds all the rows in the table right now
+  const [announcementButtonState, setAnnouncementButtonState] = useState(true);
+  const [assignmentButtonState, setAssignmentButtonState] = useState(true);
+  var [refresh, setRefresh] = useState(0);
 
   useEffect(() => {
     axios.get("http://localhost:5000/api/get-all").then((response) => {
+      console.log(response);
       setAssignments(response.data.assignments);
       setAnnouncements(response.data.announcements);
-      console.log(response.data);
-  }, [])});
+      // console.log(response.data);
+    });
+  }, [refresh]);
 
-  // const [assignments, setAssignments] = useState(refreshTable("new-assignments")); // holds all the assignments for the table
-  // const [announcements, setAnnouncements] = useState(refreshTable("new-announcements")); // holds all the announcements for the table
-  const [assignments, setAssignments] = useState([
-    {
-      id: 2,
-      course: "CPEN 212",
-      dueDate: Date.now(),
-      title: "Lab 2",
-      description: "pain and suffering :D",
-      // completed: <CheckBoxOutlineBlankIcon />,
-    },
-  ]);
-  const [announcements, setAnnouncements] = useState([
-    {
-      id: 3,
-      course: "CPSC 221",
-      dueDate: Date.now(),
-      title: "github login",
-      description: "should be easy go to canvas",
-    },
-  ]);
-  const [rows, setRows] = useState([
-    // {
-    //   id: 2,
-    //   course: "CPEN 212",
-    //   dueDate: Date.now(),
-    //   title: "Lab 2",
-    //   description: "pain and suffering :D",
-    //   // completed: <CheckBoxOutlineBlankIcon />,
-    // },
-    // {
-    //   id: 3,
-    //   course: "CPSC 221",
-    //   dueDate: Date.now(),
-    //   title: "github login",
-    //   description: "should be easy go to canvas",
-    // },
-  ]); // holds all the rows in the table right now
+  const [assignments, setAssignments] = useState([]); // holds all the assignments for the table
+  const [announcements, setAnnouncements] = useState([]); // holds all the announcements for the table
+  // const [assignments, setAssignments] = useState([
+  //   {
+  //     id: 2,
+  //     course: "CPEN 212",
+  //     dueDate: Date.now(),
+  //     title: "Lab 2",
+  //     description: "pain and suffering :D",
+  //     // completed: <CheckBoxOutlineBlankIcon />,
+  //   },
+  // ]);
+  // const [announcements, setAnnouncements] = useState([
+  //   {
+  //     id: 3,
+  //     course: "CPSC 221",
+  //     dueDate: Date.now(),
+  //     title: "github login",
+  //     description: "should be easy go to canvas",
+  //   },
+  // ]);
+  const [rows, setRows] = useState([]); // holds all the rows in the table right now
   const [row, selectRow] = useState({
     id: 0,
     course: "Welcome to TODO",
     dueDate: Date.now(),
   }); // holds the row that is selected
 
-  async function refreshTable(request) {
-    // request can be getall, new-announcements, new-assignments, mark-complete/<dataType>/<identifier>
+  function getData(request) {
     try {
-      console.log(`${request}`);
-      let result = await axios({
+      axios({
         method: "GET",
         url: `http://localhost:5000/api/${request}`,
-      })
-      .then(function (response) {
-        console.log(response);
-        // for (let i = 0; i < response.data.length; i++) {
-        //   // console.log(response.data[i]);
-        // }
-        // setRows(response.data);
+      }).then(function (response) {
+        return response.data;
       });
-      // console.log(result.response.data);
-    } catch (err) {
-      console.error(err.response.data);
-    }
+    } catch (e) {}
+  }
+
+  function refreshTable(request) {
+    // request can be getall, new-announcements, new-assignments, mark-complete/<dataType>/<identifier>
+
+    getData(request);
+
+    // console.log(result.response.data);
+
     setRows([...announcements, ...assignments]);
   }
 
@@ -101,6 +89,7 @@ function App() {
               {/* refreshbutton */}
               <IconButton
                 onClick={(e) => {
+                  setRefresh(refresh++);
                   // e.preventDefault();
                   refreshTable(`new-${view}`);
                 }}
@@ -111,32 +100,40 @@ function App() {
             </span>
             <span style={{ flexGrow: 0.5 }} />
             {/* announcement buttons */}
-            <Button
-              variant="contained"
-              onClick={() => {
-                console.log("hi");
-                setRows(announcements);
-                console.log(announcements);
-              }}
-              style={{
-                width: "200px",
-              }}
-            >
-              Announcement view
-            </Button>
-            {/* assignment button */}
-            <Button
-              variant="contained"
-              onClick={() => {
-                setRows(assignments);
-                console.log(assignments);
-              }}
-              style={{
-                width: "200px",
-              }}
-            >
-              Assignment view
-            </Button>
+            <ButtonGroup>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  console.log(announcements);
+                  setRows(announcements);
+                  setView("announcements");
+                  setAnnouncementButtonState(!announcementButtonState);
+                  setAssignmentButtonState(announcementButtonState);
+                }}
+                style={{
+                  width: "200px",
+                }}
+                disabled={announcementButtonState ? false : true}
+              >
+                Announcement view
+              </Button>
+              {/* assignment button */}
+              <Button
+                variant="contained"
+                onClick={() => {
+                  setRows(assignments);
+                  setView("assignments");
+                  setAssignmentButtonState(!assignmentButtonState);
+                  setAnnouncementButtonState(assignmentButtonState);
+                }}
+                style={{
+                  width: "200px",
+                }}
+                disabled={assignmentButtonState ? false : true}
+              >
+                Assignment view
+              </Button>
+            </ButtonGroup>
           </Stack>
         </span>
       </Grid>
