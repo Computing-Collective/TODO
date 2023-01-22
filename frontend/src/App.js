fad4beb351e59dc6970dc35ca6f79153ef89af5f
Divy Patel <82171007+divy-07@users.noreply.github.com>
2023-01-21 17:21:32 -0800

    added outline button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import DataTable from './components/DataTable';
 import ContainedButtons from './components/ContainedButton';
+import OutlinedButton from './components/OutlinedButton';
 import Stack from '@mui/material/Stack';
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
     return (
         <Grid container>
             {/* Left */}
-            <Stack className='LeftContainer' direction="column" spacing={2} style={{ backgroundColor: 'grey', overflowY: 'hidden', width: '50vw', height: '100vh'}}>
+            <Stack alignItems="center" className='LeftContainer' direction="column" spacing={2} style={{ backgroundColor: 'grey', overflowY: 'hidden', width: '50vw', height: '100vh'}}>
 
                 {/* Buttons */}
+                <span style={{padding: '40px'}}>
                 <Stack direction='row' spacing={2}>
                     <ContainedButtons 
                         text='Normal view'
@@ -22,12 +24,13 @@ function App() {
                         width='200px'
                     />
                 </Stack>
+                </span>
 
 
                 {/* Table */}
                 <DataTable
-                    cols={tempColumns}
-                    rows={tempRows}
+                    cols={cols}
+                    rows={rows}
                 />
             </Stack>
 
@@ -41,7 +44,7 @@ function App() {
 
 export default App;
 
-const tempColumns = [
+const cols = [
     { field: 'id', headerName: 'ID', width: 10 },
     { field: 'firstName', headerName: 'First name', width: 130 },
     { field: 'lastName', headerName: 'Last name', width: 130 },
@@ -55,13 +58,14 @@ const tempColumns = [
         valueGetter: (params) =>
             `${params.row.firstName || ''} ${params.row.lastName || ''}`,
     },
+    { field: 'link', headerName: 'Link', width: 130}
 ];
 
-const tempRows = [
+const rows = [
     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, link: null },
     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
---
+++ frontend/src/components/OutlinedButton.js
@@ -0,0 +1,10 @@
+    import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function OutlinedButton(props) {
+    return (
+        <Button variant="outlined" style={{ width: props.width }} >
+            {props.text}
+        </Button>
+    );
+}
